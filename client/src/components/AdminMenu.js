4d0a37e1ad5433b8cc502916572fed79490bd10f
e1010101 <123716128+complex11011@users.noreply.github.com>
2025-02-22 18:28:35 +0800

    Add tests for AdminMenu

--- client/src/components/AdminMenu.js
+++ client/src/components/AdminMenu.js
@@ -3,41 +3,51 @@ import { NavLink } from "react-router-dom";
 const AdminMenu = () => {
   return (
     <>
-      <div className="text-center">
-        <div className="list-group dashboard-menu">
-          <h4>Admin Panel</h4>
-          <NavLink
-            to="/dashboard/admin/create-category"
-            className="list-group-item list-group-item-action"
-          >
-            Create Category
-          </NavLink>
-          <NavLink
-            to="/dashboard/admin/create-product"
-            className="list-group-item list-group-item-action"
-          >
-            Create Product
-          </NavLink>
-          <NavLink
-            to="/dashboard/admin/products"
-            className="list-group-item list-group-item-action"
-          >
-            Products
-          </NavLink>
-          <NavLink
-            to="/dashboard/admin/orders"
-            className="list-group-item list-group-item-action"
-          >
-            Orders
-          </NavLink>
-          {/* <NavLink
-            to="/dashboard/admin/users"
-            className="list-group-item list-group-item-action"
-          >
-            Users
-          </NavLink> */}
-        </div>
-      </div>
+      <nav className="text-center" data-testid="admin-menu-container">
+        <h4>Admin Panel</h4>
+        <ul className="list-group dashboard-menu" aria-label="Admin Panel">
+          <li>
+            <NavLink
+              to="/dashboard/admin/create-category"
+              className="list-group-item list-group-item-action"
+            >
+              Create Category
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/dashboard/admin/create-product"
+              className="list-group-item list-group-item-action"
+            >
+              Create Product
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/dashboard/admin/products"
+              className="list-group-item list-group-item-action"
+            >
+              Products
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/dashboard/admin/orders"
+              className="list-group-item list-group-item-action"
+            >
+              Orders
+            </NavLink>
+          </li>
+          {/* <li>
+            <NavLink
+              to="/dashboard/admin/users"
+              className="list-group-item list-group-item-action"
+            >
+              Users
+            </NavLink>
+          </li> */}
+        </ul>
+      </nav>
     </>
   );
 };
