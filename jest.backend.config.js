f89f5328614cda937cd37030699cee760a64ec14
e1010101 <89738860+e1010101@users.noreply.github.com>
2025-02-25 01:00:55 +0800

    Add teardown code to close async operations

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -1,21 +1,26 @@
 export default {
   transform: {},
   // display name
-  displayName: "backend",
+  displayName: 'backend',
 
   // when testing backend
-  testEnvironment: "node",
+  testEnvironment: 'node',
 
   // which test to run
-  testMatch: ["<rootDir>/controllers/*.test.js"],
+  testMatch: [
+    // "<rootDir>/controllers/*.test.js",
+    '<rootDir>/models/*.test.js'
+  ],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["controllers/**"],
+  collectCoverageFrom: ['controllers/**', 'models/**'],
   coverageThreshold: {
     global: {
       lines: 100,
-      functions: 100,
-    },
+      functions: 100
+    }
   },
-};
+  reporters: [['summary', { summaryThreshold: 1 }]],
+  globalTeardown: '<rootDir>/tests/teardown.js'
+}
