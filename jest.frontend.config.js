404bbb6e3d7f7ddf7f5513527a35a50c7067068f
e1010101 <89738860+e1010101@users.noreply.github.com>
2025-02-25 22:07:28 +0800

    Update configs for Jest

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -1,43 +1,42 @@
 export default {
   // name displayed during tests
-  displayName: "frontend",
+  displayName: 'frontend',
 
   // simulates browser environment in jest
   // e.g., using document.querySelector in your tests
-  testEnvironment: "jest-environment-jsdom",
+  testEnvironment: 'jest-environment-jsdom',
 
   // jest does not recognise jsx files by default, so we use babel to transform any jsx files
   transform: {
-    "^.+\\.jsx?$": "babel-jest",
+    '^.+\\.jsx?$': 'babel-jest'
   },
 
   // tells jest how to handle css/scss imports in your tests
   moduleNameMapper: {
-    "\\.(css|scss)$": "identity-obj-proxy",
-    "^react-icons/(.*)$": "identity-obj-proxy"
+    '\\.(css|scss)$': 'identity-obj-proxy',
+    '^react-icons/(.*)$': 'identity-obj-proxy'
   },
 
   // ignore all node_modules except styleMock (needed for css imports)
-  transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js|react-icons)$)"],
+  transformIgnorePatterns: ['/node_modules/(?!(styleMock\\.js|react-icons)$)'],
 
   // only run these tests
-  testMatch: [
-    "<rootDir>/client/src/**/*.test.js"
-  ],
+  testMatch: ['<rootDir>/client/src/**/*.test.js'],
 
   // jest code coverage
   collectCoverage: true,
   collectCoverageFrom: [
-    "client/src/**/*.js",
-    "!client/src/**/*.test.js",
-    "!client/src/index.js",
-    "!client/src/reportWebVitals.js",
-    "!client/src/setupTests.js"
+    'client/src/**/*.js',
+    '!client/src/**/*.test.js',
+    '!client/src/index.js',
+    '!client/src/reportWebVitals.js',
+    '!client/src/setupTests.js'
   ],
   coverageThreshold: {
     global: {
       lines: 100,
-      functions: 100,
-    },
+      functions: 100
+    }
   },
-};
+  reporters: [['summary', { summaryThreshold: 1 }]]
+}
