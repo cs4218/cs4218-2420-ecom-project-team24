ca94cc769fa189a98ca3c460dee21a75d219bc11
e1010101 <89738860+e1010101@users.noreply.github.com>
2025-03-24 01:52:57 +0800

    Fix configs

--- jest.backend.config.cjs
+++ jest.backend.config.cjs
@@ -1,27 +1,36 @@
 module.exports = {
   // display name
-  displayName: "backend",
-  testEnvironment: "node",
+  displayName: 'backend',
+  testEnvironment: 'node',
 
   // which test to run
   testMatch: [
-    "<rootDir>/controllers/*.test.js",
-    "<rootDir>/models/*.test.js",
-    "<rootDir>/config/*.test.js",
-    "<rootDir>/integration-tests/**/*.test.js",
+    '<rootDir>/controllers/*.test.js',
+    '<rootDir>/models/*.test.js',
+    '<rootDir>/config/*.test.js',
+    '<rootDir>/integration-tests/**/*.test.js'
   ],
 
-  globalSetup: "<rootDir>/integration-tests/globalSetup.js",
-  globalTeardown: "<rootDir>/integration-tests/globalTeardown.js",
+  globalSetup: '<rootDir>/integration-tests/globalSetup.js',
+  globalTeardown: '<rootDir>/integration-tests/globalTeardown.js',
 
   // jest code coverage
-  //   collectCoverage: true,
-  //   collectCoverageFrom: ['controllers/**', 'models/**'],
-  //   coverageThreshold: {
-  //     global: {
-  //       lines: 20,
-  //       functions: 20
-  //     }
-  //   },
-  // reporters: [['summary', { summaryThreshold: 1 }]]
-};
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'controllers/**/*.js',
+    'models/**/*.js',
+    'helpers/**/*.js',
+    'middlewares/**/*.js',
+    'routes/**/*.js',
+    'config/**/*.js',
+    '!**/*.test.js',
+    '!**/node_modules/**'
+  ],
+  coverageThreshold: {
+    global: {
+      lines: 20,
+      functions: 20
+    }
+  },
+  coverageDirectory: 'coverage/backend'
+}
--- jest.frontend.config.cjs
+++ jest.frontend.config.cjs
@@ -1,53 +1,47 @@
 module.exports = {
   // name displayed during tests
-  displayName: "frontend",
+  displayName: 'frontend',
 
   // simulates browser environment in jest
   // e.g., using document.querySelector in your tests
-  testEnvironment: "jest-environment-jsdom",
+  testEnvironment: 'jest-environment-jsdom',
 
   // Jest setup file
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 
   // jest does not recognise jsx files by default, so we use babel to transform any jsx files
   transform: {
-    "^.+\\.jsx?$": "babel-jest",
+    '^.+\\.jsx?$': 'babel-jest'
   },
 
   // tells jest how to handle css/scss imports in your tests
   moduleNameMapper: {
-    "\\.(css|scss)$": "identity-obj-proxy",
+    '\\.(css|scss)$': 'identity-obj-proxy'
   },
 
   // ignore all node_modules except styleMock (needed for css imports)
-  transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
+  transformIgnorePatterns: ['/node_modules/(?!(styleMock\\.js)$)'],
 
   // only run these tests
   testMatch: [
-    // '<rootDir>/client/src/pages/Auth/*.test.js',
-    "<rootDir>/client/src/pages/Auth/*.test.js",
-    "<rootDir>/client/src/pages/admin/*.test.js",
-    "<rootDir>/client/src/pages/user/*.test.js",
-    "<rootDir>/client/src/pages/*.test.js",
-    "<rootDir>/client/src/components/*.test.js",
-    "<rootDir>/client/src/components/Form/*.test.js",
-    "<rootDir>/client/src/context/*.test.js",
-    "<rootDir>/client/src/hooks/*.test.js",
-    "<rootDir>/client/src/integration-tests/**/*.test.js",
+    '<rootDir>/client/src/**/**/*.test.js',
+    '<rootDir>/client/src/**/*.test.js'
   ],
 
   // jest code coverage
   collectCoverage: true,
   collectCoverageFrom: [
-    // "client/src/components/Form/*.js",
-    //     'client/src/pages/**/*.js',
-    //     '!client/src/pages/Auth/**/*.test.js',
-    //     '!client/src/pages/**/*.test.js'
+    'client/src/**/*.js',
+    'client/src/**/**/*.js',
+    '!client/src/**/**/*.test.js',
+    '!client/src/**/*.test.js',
+    '!client/src/integration-tests/*.js'
   ],
-  // coverageThreshold: {
-  //  global: {
-  // lines: 20,
-  // functions: 20,
-  //},
-  //},
-};
+  coverageThreshold: {
+    global: {
+      lines: 20,
+      functions: 20
+    }
+  },
+  coverageDirectory: 'coverage/frontend'
+}
